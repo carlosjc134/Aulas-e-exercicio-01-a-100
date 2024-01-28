80306756f0220f56a67ff2f9b19cff109729861f
José carlos <jc_solos@hotmail.com>
2024-01-28 17:50:10 -0300

    mudança

---
+++ exercicio/ex001.py
@@ -0,0 +1,4 @@
+print("Ola, Mundo!")
+
+mensagem = "Ola, Mundo!"
+print(mensagem)
---
+++ exercicio/ex002.py
@@ -0,0 +1,3 @@
+nome = input("Digite seu nome: ")
+print("É um grande prazer te conhecer,", nome, "!")
+print(f"É um grande prazer te conhecer, {nome}!")
