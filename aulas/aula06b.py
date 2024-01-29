d2ff5d1ac8457a3bc6ba98b4b5eee05c3d96bd8c
José carlos <jc_solos@hotmail.com>
2024-01-28 22:33:16 -0300

    melhorando

---
+++ aulas/aula06b.py
@@ -0,0 +1,9 @@
+n = input("Digite um valor: ")
+print(n.isnumeric()) # é númerico
+print(n.isalpha()) # é letra
+print(n.isalnum()) # é letra e número
+print(n.isupper()) # é letra maiúsculas
+print(n.islower()) # é letra minúsculas
+
+
+
--- exercicio/ex003.py
+++ exercicio/ex003.py
@@ -1,4 +1,4 @@
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite mais um número: "))
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite outro valor: "))
 s = (n1 + n2)
-print(f"a soma entre {n1} e {n2} vale {s}")
+print(f"a soma entre {n1} e {n2} é igual a {s}!")
