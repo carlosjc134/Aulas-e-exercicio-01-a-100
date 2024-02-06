b8ebae987cfdf75b6a23c1a5fc054d621fb31f9f
josé carlos <jc_solos@hotmail.com>
2024-02-06 05:31:53 -0300

    mudança

--- exercicio/ex003.py
+++ exercicio/ex003.py
@@ -1,3 +1,5 @@
+# Faça um programa que peça pra digitar dois valores, e depois mostrar uma mensagem com a soma entre eles
+
 n1 = int(input("Digite um valor: "))
 n2 = int(input("Digite outro valor: "))
 s = (n1 + n2)
