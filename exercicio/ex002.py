5d267226b787c26ab5da8c297bee121643d48015
josé carlos <jc_solos@hotmail.com>
2024-02-06 05:28:46 -0300

    mudança

--- exercicio/ex002.py
+++ exercicio/ex002.py
@@ -1,3 +1,5 @@
+# Faça um programa que peça pra digitar seu nome, e mostrar uma mensagem de boas vindas incluindo seu nome na mensagem.
+
 nome = input("Digite seu nome: ")
 print("É um grande prazer te conhecer,", nome, "!")
 print(f"É um grande prazer te conhecer, {nome}!")
