df27a39f940fbc3dfbee651acf1f191eec509584
josé carlos <jc_solos@hotmail.com>
2024-02-06 19:43:26 -0300

    mudando

---
+++ aulas/aula06c.py
@@ -0,0 +1,15 @@
+
+n1 = int(input('Um valor: '))  # se digitar o número 3
+n2 = int(input('Outro valor: '))  # se digitar o número 2
+
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+
+print(f'A soma é {s}, o resultado da multiplicação é {m} e a divisão é {d:.1f}')
+# print mostrara -> A soma é 5, o resultado da multiplicação é 6 e a divisão é 1.5
+
+print(f'Divisão inteira é {di} e a potência é {e}')
+# print mostrara -> Divisão inteira é 1 e a potencia é 9
