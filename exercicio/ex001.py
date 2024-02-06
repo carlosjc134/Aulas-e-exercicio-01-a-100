a4f476b38c7126cc793b70d703f25cc94785b5e5
josé carlos <jc_solos@hotmail.com>
2024-02-06 05:21:20 -0300

    mudança

--- exercicio/ex001.py
+++ exercicio/ex001.py
@@ -1,4 +1,6 @@
-print("Ola, Mundo!")
+# Faça um programa que mostre uma mensagem na tela
+
+print("Ola, Mundo!")  # print mostrara o que ta escrito entre parenteses na tela
 
 mensagem = "Ola, Mundo!"
-print(mensagem)
+print(mensagem)      # print mostrara o que ta escrito na variavel mensagem tela
