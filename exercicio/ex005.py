b92477d152b7df3ee2a6bcecbd61c5937499514f
josé carlos <jc_solos@hotmail.com>
2024-02-06 05:15:04 -0300

    mudança

---
+++ exercicio/ex005.py
@@ -0,0 +1,8 @@
+# Faça um programa que leia um número inteiro e mostrar na tela o seu sucessor e seu antecessor.
+
+n = int(input('Digite um número: '))  # Se digitar o número 2 mostrara 1 antecessor e 3 o sucessor.
+
+print('Analisando o valor {}, seu antecessor {} e o seu sucessor é {}.'.format(n, (n-1), (n+1)))
+
+# Resultado do print
+# Analisando o valor 2 seu antecessor é 1 e o seu sucessor é 3.
