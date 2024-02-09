0e82f69c87e17e7eeeefbd21daf9342dfccdddff
josé carlos <jc_solos@hotmail.com>
2024-02-08 21:39:15 -0300

    mudando

--- aulas/aula09b fatiamento.py
+++ aulas/aula09b fatiamento.py
@@ -1,3 +1,5 @@
+# fatiamento
+
 frase = 'Curso em Vídeo Python'
 
 mostrar = frase[9]  # -> mostrara a letra 9
@@ -8,3 +10,9 @@ print(mostrar)  # -> mostrara "Víde"
 
 mostrar = frase[9:21:2]  # mostrara as letras do 9 ao 21 pulando de 2 em 2
 print(mostrar)  # -> mostrara "VdoPto"
+
+mostrar = frase[15:]  # mostrara as letras do 15 até o final da palavra
+print(mostrar)  # -> mostrara "Python"
+
+mostrar = frase[9::3]  # mostrara as letras do 9 até o final pulando de 3 em 3
+print(mostrar)  # -> mostrara "VePh"
